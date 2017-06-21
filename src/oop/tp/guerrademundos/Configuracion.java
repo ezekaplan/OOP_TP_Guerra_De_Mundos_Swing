a49a806a97f1520671daaa75cf29655dbcff4782
ekapl1 <ezequiel.kaplan@monsanto.com>
2017-06-21 20:09:19 -0300

    2

--- src/oop/tp/guerrademundos/Configuracion.java
+++ src/oop/tp/guerrademundos/Configuracion.java
@@ -1,6 +1,7 @@
 package oop.tp.guerrademundos;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by ekapl1 on 5/22/17.
@@ -12,6 +13,14 @@ public class Configuracion {
     private JPanel panelMain;
 
     public static void main(String[] args) {
+        //open in a new thread
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
         JFrame frame = new JFrame("Guerra de Mundos!");
         frame.setContentPane(new Configuracion().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
